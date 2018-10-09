2f81f561faed4275089c2fc82d1f97df5b1552ce
Sathyananthan <sathyananthan.natarajan@cognizant.com>
2018-10-09 22:47:13 +0530

    Added test case

---
+++ ProjectManagerService/ProjectManagerService.Tests/UnitTest/MockUserData.cs
@@ -0,0 +1,86 @@
+﻿using Moq;
+using ProjectManager.BusinessLayer;
+using ProjectManager.DataAccessLayer;
+using ProjectManager.InterfaceLayer;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using CommonEntities = ProjectManager.CommonEntities;
+
+namespace ProjectManagerService.Tests
+{
+    public class MockUserData : IUserBL
+    {
+        public Collection<CommonEntities.Users> GetUsers()
+        {
+            Mock<ProjectManagerEntities> mockContext = MockDataSetList();
+            var userBL = new UserBL(mockContext.Object);
+            Collection<CommonEntities.Users> users = userBL.GetUsers();
+
+            return users;
+        }
+        public int AddUser(CommonEntities.Users user)
+        {
+            Mock<ProjectManagerEntities> mockContext = MockDataSetList();
+            var userBL = new UserBL(mockContext.Object);
+            int result = userBL.AddUser(user);
+
+            return result;
+        }
+        public CommonEntities.Users GetUserById(int userId)
+        {
+            Mock<ProjectManagerEntities> mockContext = MockDataSetList();
+            var userBL = new UserBL(mockContext.Object);
+            CommonEntities.Users user = userBL.GetUserById(userId);
+
+            return user;
+        }
+        public int UpdateUser(CommonEntities.Users user)
+        {
+            Mock<ProjectManagerEntities> mockContext = MockDataSetList();
+            var userBL = new UserBL(mockContext.Object);
+            int result = userBL.UpdateUser(user);
+
+            return result;
+        }
+
+        private static Mock<ProjectManagerEntities> MockDataSetList()
+        {
+            var data = new List<Users>()
+            {
+                new Users
+                {
+                    UserID=1,
+                    FirstName="Sathya",
+                    LastName="Natarajan",
+                    EmployeeID="433461"
+                },
+                new Users
+                {
+                    UserID=2,
+                    FirstName="Murali",
+                    LastName="S",
+                    EmployeeID="433465"
+                },
+                new Users
+                {
+                    UserID=3,
+                    FirstName="Hari",
+                    LastName="M",
+                    EmployeeID="433469"
+                }
+        }.AsQueryable();
+
+            var mockset = new Mock<DbSet<Users>>();
+            mockset.As<IQueryable<Users>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockset.As<IQueryable<Users>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockset.As<IQueryable<Users>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockset.As<IQueryable<Users>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            var mockContext = new Mock<ProjectManagerEntities>();
+            mockContext.Setup(m => m.Users).Returns(mockset.Object);
+
+            return mockContext;
+        }
+    }
+}
---
+++ ProjectManagerService/ProjectManagerService.Tests/UnitTest/UsersControllersTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Web.Http.Results;
+using NUnit.Framework;
+using ProjectManager.InterfaceLayer;
+using ProjectManagerService.Controllers;
+using System.Collections.ObjectModel;
+using ProjectMangerModel = ProjectManagerService.Models;
+
+namespace ProjectManagerService.Tests
+{
+    [TestFixture]
+    public class UsersControllersTest
+    {
+        [Test]
+        public void GetUsersTest()
+        {
+            IUserBL taskBL = new MockUserData();
+
+            var userController = new UsersController(taskBL);
+            var response = userController.GetUsers();
+            var responseResult = response as OkNegotiatedContentResult<Collection<ProjectMangerModel.Users>>;
+            Assert.IsNotNull(responseResult);
+            Assert.IsNotNull(responseResult.Content);
+            foreach (var task in responseResult.Content)
+            {
+                Assert.IsNotNull(task.UserID);
+                Assert.IsNotNull(task.FirstName);
+                Assert.IsNotNull(task.LastName);
+                Assert.IsNotNull(task.EmployeeID);
+            }
+        }
+
+        [Test]
+        public void AddUserTest_Success()
+        {
+            IUserBL userBL = new MockUserData();
+
+            // Arrange
+            var userController = new UsersController(userBL);
+
+            ProjectMangerModel.Users model = new ProjectMangerModel.Users
+            {
+                UserID = 4,
+                FirstName = "Meena",
+                LastName = "M",
+                EmployeeID = "433466"
+            };
+
+            // Act
+            var response = userController.AddUser(model);
+
+            // Assert
+            Assert.IsTrue(response is OkResult);
+        }
+
+
+    }
+}
